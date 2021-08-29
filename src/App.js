5405380ae8916101b936dbbf43e5671c66c6f688
inman <sf990668852@gmail.com>
2021-08-29 19:59:13 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useRef ,Suspense} from 'react'
 
-import { Canvas, useFrame, useThree } from '@react-three/fiber'
+import { Canvas} from '@react-three/fiber'
 
 import { useGLTF } from "@react-three/drei"
 
@@ -8,11 +8,8 @@ import gltfNodeToMesh from './controller/gltfNodeToMesh.js'
 
 const PonyCartoonModel = ()=> {
 
-  // Drei's useGLTF hook sets up draco automatically, that's how it differs from useLoader(GLTFLoader, url)
-  // { nodes, materials } are extras that come from useLoader, these do not exist in threejs/GLTFLoader
-  // nodes is a named collection of meshes, materials a named collection of materials
-  const gltf_ = useGLTF('../gltf/PonyCartoon.glb')
-  const group = useRef();
+  const gltf_ = useGLTF('../PonyCartoon.glb')
+  const group = useRef()
   
   let nodes = gltf_.nodes
   let nodesMeshOnly = Object.values(nodes).filter(data=>data.type==='Mesh')
@@ -33,11 +30,8 @@ const App=()=>{
   return( 
     <div style={{width:'100vw',height:'100vh',border:'1px solid #000'}}>
     <Canvas
-      //concurrent//這個關掉會很慘
-      pixelRatio={[1, 2]}
+      concurrent//這個關掉會很慘
       camera={{position:[0,0,13],fov:40}}
-      shadowMap
-      colorManagement
     >
       <ambientLight 
         intensity={0.3}
