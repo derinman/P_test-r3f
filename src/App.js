7ad3ddfe30ec96d3735abfea0d4c856661396855
inman <sf990668852@gmail.com>
2021-09-01 15:35:07 +0800

    gltf test

--- src/App.js
+++ src/App.js
@@ -1,56 +1,57 @@
-import React, {useState,useEffect, useRef ,Suspense} from 'react'
+import React, {useRef ,Suspense} from 'react'
 
 import { Canvas} from '@react-three/fiber'
-
-import { useGLTF } from "@react-three/drei"
-
-
+import { useGLTF, OrbitControls } from "@react-three/drei"
 
 import gltfNodeToMesh from './controller/gltfNodeToMesh.js'
 import dumpObject from './controller/dump.js'
 
-import url from './glb/ponycartoon.glb'
+// import glbUrl from './glb/ponycartoon.glb'
+import glbUrl from './glb/polly.glb'
 
-const PonyCartoonModel = (props)=> {
+const PonyCartoonModel = ()=> {
   
-  const gltf = useGLTF(url)
+  const glb = useGLTF(glbUrl)
+  console.log(glb)
   const group = useRef()
-  console.log(dumpObject(gltf.scene).join('\n'))
-
-  let nodes = gltf.nodes
-  let nodesMeshOnly = Object.values(nodes).filter(data=>data.type==='Mesh')
+  console.log(dumpObject(glb.scene).join('\n'))
+  
+  const nodes = glb.nodes
+  
+  const nodesMeshOnly = Object.values(nodes).filter(data=>data.type==='Mesh')
 
   return (
     <group
       ref={group}
     >
-      {gltfNodeToMesh(nodesMeshOnly)}
+      <primitive object={glb.scene} />
+      {/* {gltfNodeToMesh(nodesMeshOnly)} */}
     </group>
   )
 }
 
 const App=()=>{
-
-
   return( 
     <div style={{width:'100vw',height:'100vh'}}>
-    <Canvas
-      concurrent//這個關掉會很慘
-      camera={{position:[0,0,13],fov:40}}
-    >
-      <ambientLight 
-        intensity={0.3}
-        color={'#42b8eb'}
-      />
+      <Canvas
+        concurrent//這個關掉會很慘
+        camera={{position:[0,0,2],fov:30}}
+      >
+        <ambientLight 
+          intensity={1}
+          color={'#FFF'}
+        />
         <Suspense fallback={null}>
-          <PonyCartoonModel/>
-        </Suspense> 
-    </Canvas>
-    
+          <PonyCartoonModel />
+        </Suspense>
+        <OrbitControls minPolarAngle={Math.PI / 2} maxPolarAngle={Math.PI / 2} enableZoom={false} enablePan={false} />
+      </Canvas>
     </div>
-            )
+  )
 }
 
-export default App;
+export default App
 
 //useGLTF.preload('/glb/ponycartoon.glb')
+
+
