a76d6370c8fd325d253b35089167408bb3495336
inman <sf990668852@gmail.com>
2021-09-01 16:38:13 +0800

    r3f test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, {useRef ,Suspense} from 'react'
 
 import { Canvas} from '@react-three/fiber'
-import { useGLTF, OrbitControls } from "@react-three/drei"
+import { useGLTF, OrbitControls,PerspectiveCamera } from "@react-three/drei"
+
+import * as THREE from 'three'
 
 import gltfNodeToMesh from './controller/gltfNodeToMesh.js'
 import dumpObject from './controller/dump.js'
 
-// import glbUrl from './glb/ponycartoon.glb'
-import glbUrl from './glb/polly.glb'
+import glbUrl from './glb/ponycartoon.glb'
+// import glbUrl from './glb/polly.glb'
 
 const PonyCartoonModel = ()=> {
   
@@ -31,12 +33,14 @@ const PonyCartoonModel = ()=> {
 }
 
 const App=()=>{
+  
+  const mainCamera = useRef()
+  const controls = useRef()
+
   return( 
     <div style={{width:'100vw',height:'100vh'}}>
-      <Canvas
-        concurrent//這個關掉會很慘
-        camera={{position:[0,0,2],fov:30}}
-      >
+      <Canvas concurrent>
+
         <ambientLight 
           intensity={1}
           color={'#FFF'}
@@ -44,7 +48,33 @@ const App=()=>{
         <Suspense fallback={null}>
           <PonyCartoonModel />
         </Suspense>
-        <OrbitControls minPolarAngle={Math.PI / 2} maxPolarAngle={Math.PI / 2} enableZoom={false} enablePan={false} />
+        <PerspectiveCamera
+          makeDefault
+          ref={mainCamera}
+          position-x={ 0}
+          position-y={0}
+          position-z={15}
+          up={[0, 0, 1]}//世界座標的向量
+          fov={ 30 }
+          //aspect={ width / height }
+          near={ 1 }
+          far={ 10000 }
+          visible={false}
+          controls={controls}
+        />
+        <OrbitControls
+          ref={controls}
+          camera={mainCamera.current}
+          enabled={true}
+          enablePan={true}
+          enableZoom={true}
+          enableRotate={true}
+          minPolarAngle={Math.PI / 2} 
+          maxPolarAngle={Math.PI / 2}
+          target-x={0}
+          target-y={0}
+          target-z={0}
+        />
       </Canvas>
     </div>
   )
@@ -52,6 +82,5 @@ const App=()=>{
 
 export default App
 
-//useGLTF.preload('/glb/ponycartoon.glb')
-
+// //useGLTF.preload('/glb/ponycartoon.glb')
 
