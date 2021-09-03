7a774f1dec7e40cc730d4d6c521fc0c5fc6d9000
inman <sf990668852@gmail.com>
2021-09-03 20:26:34 +0800

    spotLight component

--- src/App.js
+++ src/App.js
@@ -1,102 +1,314 @@
-import React, {useState, useRef ,Suspense, useEffect} from 'react'
+import React, { useState, useRef, Suspense, useEffect } from "react";
 
-import { Canvas} from '@react-three/fiber'
-import { useGLTF, OrbitControls,PerspectiveCamera } from "@react-three/drei"
+import { Canvas } from "@react-three/fiber";
+import { useGLTF, OrbitControls, PerspectiveCamera } from "@react-three/drei";
 
-import Slider from 'react-rangeslider'
-import 'react-rangeslider/lib/index.css'
+import Slider from "react-rangeslider";
+import "react-rangeslider/lib/index.css";
 
-import { SketchPicker } from 'react-color'
+import { SketchPicker } from "react-color";
 
-import gltfNodeToMesh from './helper/gltfNodeToMesh.js'
-import dumpObject from './helper/dump.js'
+import gltfNodeToMesh from "./helper/gltfNodeToMesh.js";
+import dumpObject from "./helper/dump.js";
 
 // import glbUrl from './glb/ponycartoon.glb'
-// import glbUrl from './glb/polly.glb'
-import glbUrl from './glb/dinosaur.glb'
+import glbUrl from "./glb/polly.glb";
+// import glbUrl from './glb/dinosaur.glb'
 
-const PonyCartoonModel = ()=> {
-  
-  const glb = useGLTF(glbUrl)
-  console.log('glb/gltf:',glb)
-  const group = useRef()
-  console.log(dumpObject(glb.scene).join('\n'))
-  
-  const nodes = glb.nodes
-  
-  
+const PonyCartoonModel = () => {
+  const glb = useGLTF(glbUrl);
+  // console.log('glb/gltf:',glb)
+  const group = useRef();
+  // console.log(dumpObject(glb.scene).join('\n'))
+
+  const nodes = glb.nodes;
 
   return (
-    <group
-      ref={group}
-    >
+    <group ref={group}>
       {/* <primitive object={glb.scene} /> */}
       {gltfNodeToMesh(nodes)}
     </group>
-  )
-}
+  );
+};
 
-const App=()=>{
-  
-  const [up, setUp] = useState([0,1,0])
+const SpotLight = (props) => {
+  const { spotLightConfig } = props;
+  const targetRef = useRef();
 
-  const [ptLight1, setptLight1]= useState(
-    { x:0,
-      y:1,
-      z:0, 
-      intensity:5,
-      color:'#FFFFFF',
-      distance:0,
-      penumbra:0.5,
-      angle:Math.PI/7,
-      targetX:0,
-      targetY:0,
-      targetZ:0
-    }
-  )
+  return (
+    <>
+      <spotLight
+        // ref={}
+        position={[spotLightConfig.x, spotLightConfig.y, spotLightConfig.z]}
+        color={spotLightConfig.color}
+        distance={spotLightConfig.distance} //Default is 0 (no limit)
+        penumbra={spotLightConfig.penumbra} //values between zero and 1. Default is zero.
+        angle={spotLightConfig.angle} //upper bound is Math.PI/2
+        intensity={spotLightConfig.intensity} //Default is 1
+        decay={2}
+        castShadow
+        shadow-mapSize-height={1024 / 512} //試試1024/500~1024
+        shadow-mapSize-width={1024 / 512} //試試1024/500~1024
+        shadow-bias={0.05} //試試0.01~0.07
+        shadow-focus={0.001} //試試0.1~2
+        target={targetRef.current}
+        visible={true}
+      />
+      <mesh
+        visible
+        position={[spotLightConfig.x, spotLightConfig.y, spotLightConfig.z]}
+        rotation={[0, 0, 0]}
+        castShadow
+      >
+        <sphereGeometry attach="geometry" args={[0.1, 16, 16]} />
+        <meshStandardMaterial
+          attach="material"
+          color="white"
+          wireframe={true}
+        />
+      </mesh>
+      <mesh
+        ref={targetRef}
+        visible
+        position={[
+          spotLightConfig.targetX,
+          spotLightConfig.targetY,
+          spotLightConfig.targetZ,
+        ]}
+        rotation={[0, 0, 0]}
+        castShadow
+      >
+        <sphereGeometry attach="geometry" args={[0.1, 16, 16]} />
+        <meshStandardMaterial
+          attach="material"
+          color="white"
+          wireframe={true}
+        />
+      </mesh>
+    </>
+  );
+};
 
-  const canvasRef = useRef()
-  const mainCameraRef = useRef()
-  const axesHelperRef = useRef()
-  const controlsRef = useRef()
-  const spotLight1Ref = useRef()
-  const targetRef = useRef()
+const SpotLightGUI = (props) => {
+  const { name, spotLightConfig, setSpotLightConfig } = props;
+  const [isClose, setIsClose] = useState(true)
 
-  useEffect(()=>{
+  return (
+    <>
+      <div
+        onClick={()=>setIsClose(c=>!c)}
+      >
+        {name}
+      </div>
+        
+
+      {!isClose &&<>
+        <div
+        onClick={()=>console.log(spotLightConfig)}
+      >
+        snapshot
+      </div>
+      <Slider
+        tooltip={false}
+        handleLabel={spotLightConfig.x}
+        value={spotLightConfig.x}
+        step={0.25}
+        max={20}
+        min={-10}
+        orientation="horizontal"
+        onChange={(e) => setSpotLightConfig({ ...spotLightConfig, x: e })}
+      />
+      <Slider
+        tooltip={false}
+        handleLabel={spotLightConfig.y}
+        value={spotLightConfig.y}
+        step={0.25}
+        max={20}
+        min={-10}
+        orientation="horizontal"
+        onChange={(e) => setSpotLightConfig({ ...spotLightConfig, y: e })}
+      />
+      <Slider
+        tooltip={false}
+        handleLabel={spotLightConfig.z}
+        value={spotLightConfig.z}
+        step={0.25}
+        max={20}
+        min={-10}
+        orientation="horizontal"
+        onChange={(e) => setSpotLightConfig({ ...spotLightConfig, z: e })}
+      />
+      intensity
+      <Slider
+        tooltip={false}
+        handleLabel={spotLightConfig.intensity}
+        value={spotLightConfig.intensity}
+        step={0.25}
+        max={10}
+        min={0}
+        orientation="horizontal"
+        onChange={(e) =>
+          setSpotLightConfig({ ...spotLightConfig, intensity: e })
+        }
+      />
+      Distance
+      <Slider
+        tooltip={false}
+        handleLabel={spotLightConfig.distance}
+        value={spotLightConfig.distance}
+        step={0.25}
+        max={5}
+        min={0}
+        orientation="horizontal"
+        onChange={(e) =>
+          setSpotLightConfig({ ...spotLightConfig, distance: e })
+        }
+      />
+      Penumbra
+      <Slider
+        tooltip={false}
+        handleLabel={spotLightConfig.penumbra}
+        value={spotLightConfig.penumbra}
+        step={0.1}
+        max={1}
+        min={0}
+        orientation="horizontal"
+        onChange={(e) =>
+          setSpotLightConfig({ ...spotLightConfig, penumbra: e })
+        }
+      />
+      Angle
+      <Slider
+        tooltip={false}
+        handleLabel={spotLightConfig.angle}
+        value={spotLightConfig.angle}
+        step={0.1}
+        max={Math.PI / 2}
+        min={0}
+        orientation="horizontal"
+        onChange={(e) => setSpotLightConfig({ ...spotLightConfig, angle: e })}
+      />
+      Target
+      <Slider
+        tooltip={false}
+        handleLabel={spotLightConfig.targetX}
+        value={spotLightConfig.targetX}
+        step={0.25}
+        max={10}
+        min={-10}
+        orientation="horizontal"
+        onChange={(e) => setSpotLightConfig({ ...spotLightConfig, targetX: e })}
+      />
+      <Slider
+        tooltip={false}
+        handleLabel={spotLightConfig.targetY}
+        value={spotLightConfig.targetY}
+        step={0.25}
+        max={10}
+        min={-10}
+        orientation="horizontal"
+        onChange={(e) => setSpotLightConfig({ ...spotLightConfig, targetY: e })}
+      />
+      <Slider
+        tooltip={false}
+        handleLabel={spotLightConfig.targetZ}
+        value={spotLightConfig.targetZ}
+        step={0.25}
+        max={10}
+        min={-10}
+        orientation="horizontal"
+        onChange={(e) => setSpotLightConfig({ ...spotLightConfig, targetZ: e })}
+      />
+      <SketchPicker
+        color={spotLightConfig.color}
+        onChangeComplete={(e) =>
+          setSpotLightConfig({ ...spotLightConfig, color: e.hex })
+        }
+        presetColors={[]}
+      />
+      </>}
+    </>
+  );
+};
+
+const App = () => {
+  const [up, setUp] = useState([0, 1, 0]);
+
+  const [spotLight1, setSpotLight1] = useState({
+    x: 0,
+    y: 1,
+    z: 0,
+    intensity: 5,
+    color: "#FFFFFF",
+    distance: 0,
+    penumbra: 0.5,
+    angle: Math.PI / 7,
+    targetX: 0,
+    targetY: 0,
+    targetZ: 0,
+  });
+
+  const [spotLight2, setSpotLight2] = useState({
+    x: 1,
+    y: 1,
+    z: 0,
+    intensity: 5,
+    color: "#FFFFFF",
+    distance: 0,
+    penumbra: 0.5,
+    angle: Math.PI / 7,
+    targetX: 0,
+    targetY: 0,
+    targetZ: 0,
+  });
+
+  const canvasRef = useRef();
+  const mainCameraRef = useRef();
+  const axesHelperRef = useRef();
+  const controlsRef = useRef();
+
+  useEffect(() => {
     //console.log(axesHelperRef)
-    console.log(spotLight1Ref)
-  },
-    []
-  )
-  
+    // console.log(spotLight1Ref)
+    // console.log(mainCameraRef)
+  }, []);
 
-  return( 
-    <div style={{width:'100vw',height:'100vh'}}>
+  return (
+    <div 
+      style={{ 
+        position:'relative',
+        width: "100%", 
+        height: "100%",
+        top:'0px',
+        left:'0px',
+        overflow:'hidden',
+        // border:'1px solid #000'  
+    }}>
       <Canvas 
-        ref={canvasRef}
+        ref={canvasRef} 
         concurrent
       >
         <Suspense fallback={null}>
           <PonyCartoonModel />
         </Suspense>
-        <axesHelper 
+        <axesHelper
           ref={axesHelperRef}
-          scale={[10,10,10]}
-          up={up}//世界座標的向量
+          scale={[10, 10, 10]}
+          up={up} //世界座標的向量
         />
         <PerspectiveCamera
           makeDefault
           ref={mainCameraRef}
-          position-x={ 0}
-          position-y={0}
+          controls={controlsRef.current}
+          position-x={0}
+          position-y={-0}
           position-z={25}
-          up={up}//世界座標的向量
-          fov={ 30 }
+          up={up} //世界座標的向量
+          fov={30}
           //aspect={ width / height }
-          near={ 1 }
-          far={ 10000 }
+          near={1}
+          far={10000}
           visible={false}
-          controls={controlsRef}
         />
         <OrbitControls
           ref={controlsRef}
@@ -105,187 +317,48 @@ const App=()=>{
           enablePan={true}
           enableZoom={true}
           enableRotate={true}
-          // minPolarAngle={Math.PI / 2} 
+          // minPolarAngle={Math.PI / 2}
           // maxPolarAngle={Math.PI / 2}
           target-x={0}
           target-y={0}
           target-z={0}
         />
-        <ambientLight 
-          intensity={1}
-          color={'#FFF'}
-        />
-        <hemisphereLight 
-          intensity={0.7}
-          skyColor={'#ff0800'} 
-          groundColor={'#f77b77'} 
+        <ambientLight intensity={0.3} color={"#FFF"} />
+        <hemisphereLight
+          intensity={0.5}
+          skyColor={"#ff0800"}
+          groundColor={"#f77b77"}
           castShadow
         />
-        <spotLight
-            ref={spotLight1Ref}
-            position={[ptLight1.x,ptLight1.y,ptLight1.z]}
-            color={ptLight1.color}
-            distance={ptLight1.distance}//Default is 0 (no limit)
-            penumbra={ptLight1.penumbra}//values between zero and 1. Default is zero.
-            angle={ptLight1.angle}//upper bound is Math.PI/2
-            intensity={ptLight1.intensity}//Default is 1
-            decay={2}
-            castShadow
-            shadow-mapSize-height={1024/512}//試試1024/500~1024
-            shadow-mapSize-width={1024/512}//試試1024/500~1024
-            shadow-bias={0.05}//試試0.01~0.07
-            shadow-focus={0.001}//試試0.1~2
-            target={targetRef.current}
-            visible={true}
-        />
-        <mesh
-          visible
-          position={[ptLight1.x,ptLight1.y,ptLight1.z]}
-          rotation={[0, 0, 0]}
-          castShadow
-        >
-          <sphereGeometry attach="geometry" args={[0.1, 16, 16]} />
-          <meshStandardMaterial
-            attach="material"
-            color="white"
-            wireframe={true}
-          />
-        </mesh>
-        <mesh
-          ref={targetRef}
-          visible
-          position={[ptLight1.targetX,ptLight1.targetY,ptLight1.targetZ]}
-          rotation={[0, 0, 0]}
-          castShadow
-        >
-          <sphereGeometry attach="geometry" args={[0.1, 16, 16]} />
-          <meshStandardMaterial
-            attach="material"
-            color="white"
-            wireframe={true}
-          />
-        </mesh>
+        <SpotLight spotLightConfig={spotLight1} />
+        <SpotLight spotLightConfig={spotLight2} />
       </Canvas>
-      
-      <div style={{position:'absolute',top:'0.5%',right:2,width:250,height:'99%', border:'1px solid #000'}}>
-        ptLightPos1
-        <Slider
-          tooltip={false}
-          handleLabel={ptLight1.x}
-          value={ptLight1.x}
-          step={0.25}
-          max={20}
-          min={-10}
-          orientation="horizontal"
-          onChange={e=>setptLight1({...ptLight1,x:e})}
+
+      <div
+        style={{
+          position: "absolute",
+          top: "2.5vh",
+          right: "2.5vh",
+          width: '250px',
+          height: "95%",
+          border: "1px solid #000"
+        }}
+      >
+        <SpotLightGUI
+          name = {'spotLight1'}
+          spotLightConfig={spotLight1}
+          setSpotLightConfig={setSpotLight1}
         />
-        <Slider
-          tooltip={false}
-          handleLabel={ptLight1.y}
-          value={ptLight1.y}
-          step={0.25}
-          max={20}
-          min={-10}
-          orientation="horizontal"
-          onChange={e=>setptLight1({...ptLight1,y:e})}
-        />
-        <Slider
-          tooltip={false}
-          handleLabel={ptLight1.z}
-          value={ptLight1.z}
-          step={0.25}
-          max={20}
-          min={-10}
-          orientation="horizontal"
-          onChange={e=>setptLight1({...ptLight1,z:e})}
-        />
-        intensity
-        <Slider
-          tooltip={false}
-          handleLabel={ptLight1.intensity}
-          value={ptLight1.intensity}
-          step={0.25}
-          max={10}
-          min={0}
-          orientation="horizontal"
-          onChange={e=>setptLight1({...ptLight1,intensity:e})}
-        />
-        Distance
-        <Slider
-          tooltip={false}
-          handleLabel={ptLight1.distance}
-          value={ptLight1.distance}
-          step={0.25}
-          max={5}
-          min={0}
-          orientation="horizontal"
-          onChange={e=>setptLight1({...ptLight1,distance:e})}
-        />
-        Penumbra
-        <Slider
-          tooltip={false}
-          handleLabel={ptLight1.penumbra}
-          value={ptLight1.penumbra}
-          step={0.1}
-          max={1}
-          min={0}
-          orientation="horizontal"
-          onChange={e=>setptLight1({...ptLight1,penumbra:e})}
-        />
-        Angle
-        <Slider
-          tooltip={false}
-          handleLabel={ptLight1.angle}
-          value={ptLight1.angle}
-          step={0.1}
-          max={Math.PI/2}
-          min={0}
-          orientation="horizontal"
-          onChange={e=>setptLight1({...ptLight1,angle:e})}
-        />
-        Target
-        <Slider
-          tooltip={false}
-          handleLabel={ptLight1.targetX}
-          value={ptLight1.targetX}
-          step={0.25}
-          max={10}
-          min={-10}
-          orientation="horizontal"
-          onChange={e=>setptLight1({...ptLight1,targetX:e})}
-        />
-        <Slider
-          tooltip={false}
-          handleLabel={ptLight1.targetY}
-          value={ptLight1.targetY}
-          step={0.25}
-          max={10}
-          min={-10}
-          orientation="horizontal"
-          onChange={e=>setptLight1({...ptLight1,targetY:e})}
-        />
-        <Slider
-          tooltip={false}
-          handleLabel={ptLight1.targetZ}
-          value={ptLight1.targetZ}
-          step={0.25}
-          max={10}
-          min={-10}
-          orientation="horizontal"
-          onChange={e=>setptLight1({...ptLight1,targetZ:e})}
-        />
-        <SketchPicker
-          color={ ptLight1.color }
-          onChangeComplete={ e=>setptLight1({...ptLight1,color:e.hex}) }
-          presetColors={[]}
+        <SpotLightGUI
+          name = {'spotLight2'}
+          spotLightConfig={spotLight2}
+          setSpotLightConfig={setSpotLight2}
         />
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
-useGLTF.preload(glbUrl)
-
+useGLTF.preload(glbUrl);
--- src/index.js
+++ src/index.js
@@ -1,18 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import { createGlobalStyle } from 'styled-components'
 
+const GlobalStyle = createGlobalStyle`
+    *{
+        font-family: 'MuseoModerno', 'Arial', 'sans-serif', 'Helvetica', '黑體-繁', '微軟正黑體'; !important;
+    };
+    html {
+        /*網頁版版 1rem 基準*/
+        font-size: 16px;
+        /*行動版 1rem 基準*/
+        @media (max-width: 768px) {
+            font-size: 14px;
+        }
+    };
+    #root {
+        width: 100vw;
+        height: 100vh;
+    }
+    body {
+        width:100%;
+        height:100%;
+        margin: 0;
+        overflow:hidden;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        -webkit-touch-callout: none;
+        -webkit-user-select: none;
+        -khtml-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+    };
+`
 
 ReactDOM.render(
-  <React.StrictMode>
+  <>
+    <GlobalStyle/>
     <App />
-  </React.StrictMode>,
+  </>,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
