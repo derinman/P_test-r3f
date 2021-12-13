c61162e9ca8b5c9bafc35d67dabf326315866ada
inman <sf990668852@gmail.com>
2021-12-13 16:22:25 +0800

    update

--- src/devModel/No001.js
+++ src/devModel/No001.js
@@ -1,33 +1,23 @@
-import React, { useRef } from "react";
+import React from "react";
 
-import {
-  useGLTF,
-} from "@react-three/drei";
-
+import { useGLTF } from "@react-three/drei";
 
 import gltfNodeToMesh from "../helper/gltfNodeToMesh.js";
 import dumpObject from "../helper/dump.js";
 
-import glbUrl from '../glb/no001.glb'
+import glbUrl from "../glb/no001.glb";
 // import glbUrl from "../glb/polly.glb";
 // import glbUrl from './glb/dinosaur.glb'
 
 const No001 = () => {
   const glb = useGLTF(glbUrl);
-  // console.log('glb/gltf:',glb)
-  const group = useRef();
-  // console.log(dumpObject(glb.scene).join('\n'))
-
   const nodes = glb.nodes;
 
-  return (
-    <group ref={group}>
-      {/* <primitive object={glb.scene} /> */}
-      {gltfNodeToMesh(nodes)}
-    </group>
-  );
+  // console.log(dumpObject(glb.scene).join('\n'))
+
+  return <group>{gltfNodeToMesh(nodes)}</group>;
 };
 
 useGLTF.preload(glbUrl);
 
-export default No001
+export default No001;
--- src/devModel/PonyCartoon.js
+++ src/devModel/PonyCartoon.js
@@ -1,39 +1,21 @@
-import React, { useState, useRef, Suspense, useEffect } from "react";
+import React from "react";
 
-import * as THREE from "three";
-import { Canvas } from "@react-three/fiber";
-import {
-  useGLTF,
-  OrbitControls,
-  PerspectiveCamera,
-  softShadows,
-} from "@react-three/drei";
-
+import { useGLTF } from "@react-three/drei";
 
 import gltfNodeToMesh from "../helper/gltfNodeToMesh.js";
 import dumpObject from "../helper/dump.js";
 
-import glbUrl from '../glb/ponycartoon.glb'
-// import glbUrl from "../glb/polly.glb";
-// import glbUrl from './glb/dinosaur.glb'
-
+import glbUrl from "../glb/ponycartoon.glb";
 
 const PonyCartoon = () => {
   const glb = useGLTF(glbUrl);
-  // console.log('glb/gltf:',glb)
-  const group = useRef();
-  // console.log(dumpObject(glb.scene).join('\n'))
-
   const nodes = glb.nodes;
 
-  return (
-    <group ref={group}>
-      {/* <primitive object={glb.scene} /> */}
-      {gltfNodeToMesh(nodes)}
-    </group>
-  );
+  // console.log(dumpObject(glb.scene).join('\n'))
+
+  return <group>{gltfNodeToMesh(nodes)}</group>;
 };
 
 useGLTF.preload(glbUrl);
 
-export default PonyCartoon
+export default PonyCartoon;
