b99970a04ec5196d677068061e47b87f27814f95
inman <sf990668852@gmail.com>
2021-12-13 16:30:29 +0800

    add scifiCorridor

---
+++ src/devModel/ScifiCorridor.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import { useGLTF } from "@react-three/drei";
+
+import gltfNodeToMesh from "../helper/gltfNodeToMesh.js";
+import dumpObject from "../helper/dump.js";
+
+import glbUrl from "../glb/scifiCorridor.glb";
+
+const ScifiCorridor = () => {
+  const glb = useGLTF(glbUrl);
+  const nodes = glb.nodes;
+
+  // console.log(dumpObject(glb.scene).join('\n'))
+
+  return <group>{gltfNodeToMesh(nodes)}</group>;
+};
+
+useGLTF.preload(glbUrl);
+
+export default ScifiCorridor;
--- src/devModel/index.js
+++ src/devModel/index.js
@@ -4,3 +4,4 @@ import { lazy } from 'react'
 export const pollyDog  = {name:'PollyDog',Component: lazy(() => import('./PollyDog.js')),dev:false,default:true}
 export const ponyCartoon    = {name:'PonyCartoon',Component: lazy(() => import('./PonyCartoon.js')),dev:false,default:false}
 export const no001    = {name:'No001',Component: lazy(() => import('./No001.js')),dev:false,default:false}
+export const scifiCorridor    = {name:'ScifiCorridor',Component: lazy(() => import('./ScifiCorridor.js')),dev:false,default:false}
