7189940a03eb1851020c0bdb2753497b1b22d5d0
inman <sf990668852@gmail.com>
2022-01-15 14:46:30 +0800

    update

--- src/devModel/index.js
+++ src/devModel/index.js
@@ -1,11 +1,7 @@
 import { lazy } from "react";
 
 //dev:false(開發完成); dev:true(開發中)
-export const pollyDog = {
-  name: "PollyDog",
-  Component: lazy(() => import("./PollyDog.js")),
-  camLightTest: true,
-};
+
 export const ponyCartoon = {
   name: "PonyCartoon",
   Component: lazy(() => import("./PonyCartoon.js")),
@@ -20,6 +16,11 @@ export const no002 = {
   name: "No002",
   Component: lazy(() => import("./No002.js")),
   camLightTest: false,
+};
+export const pollyDog = {
+  name: "PollyDog",
+  Component: lazy(() => import("./PollyDog.js")),
+  camLightTest: true,
 };
 export const alphaTest = {
   name: "AlphaTest",
