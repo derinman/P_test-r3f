ca73141d8a06efab5db51a4570e54bd0747af2d4
inman <sf990668852@gmail.com>
2022-01-25 14:15:44 +0800

    update

--- src/devModel/No004.js
+++ src/devModel/No004.js
@@ -1,31 +1,58 @@
-import React from "react";
+import React, { useRef } from "react";
 
 import {
   useGLTF,
+  OrbitControls,
+  PerspectiveCamera,
   Sky,
   Stars,
   Environment,
 } from "@react-three/drei";
 
 import gltfNodeToMesh from "./helper/gltfNodeToMesh.js";
-import dumpObject from "./helper/dump.js";
+// import dumpObject from "./helper/dump.js";
 
 import glbUrl from "./glb/no004.glb";
 
-
+import cameraJson from "./camLightConfig/No004/camera.json";
 
 const No004 = () => {
   const glb = useGLTF(glbUrl);
   const nodes = glb.nodes;
-
   //console.log(dumpObject(glb.scene).join("\n"));
-  //console.log(nodes);
 
+  const mainCameraRef = useRef();
+  const controlsRef = useRef();
 
   return (
     <group>
       {gltfNodeToMesh(nodes)}
-
+      <PerspectiveCamera
+        ref={mainCameraRef}
+        controls={controlsRef.current}
+        makeDefault={true}
+        visible={false}
+        up={[0, 1, 0]} //世界座標的向量
+        position={cameraJson.position}
+        fov={cameraJson.fov}
+        near={cameraJson.near}
+        far={cameraJson.far}
+      />
+      <OrbitControls
+        ref={controlsRef}
+        camera={mainCameraRef.current}
+        enabled={cameraJson.enabled}
+        enablePan={cameraJson.enablePan}
+        enableZoom={cameraJson.enableZoom}
+        enableRotate={cameraJson.enableRotate}
+        maxPolarAngle={cameraJson.maxPolarAngle}
+        minPolarAngle={cameraJson.minPolarAngle}
+        maxAzimuthAngle={cameraJson.maxAzimuthAngle}
+        minAzimuthAngle={cameraJson.minAzimuthAngle}
+        maxDistance={cameraJson.maxDistance}
+        minDistance={cameraJson.minDistance}
+        target={cameraJson.orbitTarget}
+      />
       <Sky
         distance={450000}
         sunPosition={[1, 1, 0]}
